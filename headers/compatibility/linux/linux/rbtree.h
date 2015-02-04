4e4e2a75e3b95d77b793abb9cc735d8040ca9002
Hamish Morrison <hamishm53@gmail.com>
2015-02-04 19:21:57 +0000

    Linux compat: start rbtree implementation

--- headers/compatibility/linux/linux/rbtree.h
+++ headers/compatibility/linux/linux/rbtree.h
@@ -2,8 +2,32 @@
  * Copyright 2015, Hamish Morrison, hamishm53@gmail.com.
  * All rights reserved. Distributed under the terms of the MIT License.
  */
-#ifndef _H
-#define _H
+#ifndef _LINUX_RBTREE_H
+#define _LINUX_RBTREE_H
+
+
+#define RB_NONE		0
+#define RB_RED		1
+#define RB_BLACK 	2
+
+
+struct rb_node {
+	int colour;
+	struct rb_node* parent;
+	struct rb_node* rb_left;
+	struct rb_node* rb_right;
+};
+
+#define rb_parent(r) 	((struct rb_node*)((r)->parent))
+#define rb_color(r) 	((struct rb_node*)((r)->colour))
+
+#define rb_is_red(r)	(rb_color(r) == RB_RED)
+#define rb_is_black(r)	(rb_color(r) == RB_BLACK)
+
+#define rb_set_parent(r, p)	(rb_parent(r) = (p))
+#define rb_set_color(r, c)	(rb_color(r) = (c))
+
+#define rb_entry(ptr, type, member)	container_of(ptr, type, member)
 
 
 #endif
